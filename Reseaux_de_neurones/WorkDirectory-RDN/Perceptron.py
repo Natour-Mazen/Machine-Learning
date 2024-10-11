c847309ec07f93b0fe2f80207262245e0941ae1a
Léo Hervouet <leoohervouet@gmail.com>
2024-10-11 08:52:45 +0200

    Add perceptron_simple

---
+++ Reseaux_de_neurones/WorkDirectory-RDN/Perceptron.py
@@ -0,0 +1,10 @@
+
+
+def perceptron_simple(x : list, w : float, active : int = 0) -> list:
+    """
+     Perceptron simple.
+    :param x: Synaptic weights of the neuron, vec3.
+    :param w: The input of the neural network
+    :param active:
+    :return:
+    """
