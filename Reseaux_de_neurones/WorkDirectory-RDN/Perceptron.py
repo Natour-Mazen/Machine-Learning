e4c8ca41be70c22660ff20a329411a28fd12d72d
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-10-11 10:00:49 +0200

    Update Perceptron.py
    perceptron simple

--- Reseaux_de_neurones/WorkDirectory-RDN/Perceptron.py
+++ Reseaux_de_neurones/WorkDirectory-RDN/Perceptron.py
@@ -1,6 +1,7 @@
+import numpy as np
 
 
-def perceptron_simple(x : list, w : float, active : int = 0) -> list:
+def perceptron_simple(x: list, w: np.ndarray, active: int = 0) -> list:
     """
      Perceptron simple.
     :param x: Synaptic weights of the neuron, vec3.
@@ -8,3 +9,9 @@ def perceptron_simple(x : list, w : float, active : int = 0) -> list:
     :param active:
     :return:
     """
+    seuil = w[0]
+    dot_product = np.dot(x, w[1:])
+    active = dot_product + seuil
+    return np.sign(x) if (active == 0) else np.tanh(x)
+
+
