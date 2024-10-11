f1ddef2f43c8a895dd99a8aab3f851f34d8321a7
Léo Hervouet <leoohervouet@gmail.com>
2024-10-11 09:12:47 +0200

    Change perceptron

--- Reseaux_de_neurones/WorkDirectory-RDN/Perceptron.py
+++ Reseaux_de_neurones/WorkDirectory-RDN/Perceptron.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 
-def perceptron_simple(x: list, w: np.ndarray, active: int) -> list:
+def perceptron_simple(x: np.array, w: np.array, active: int) -> list:
     """
      Perceptron simple.
     :param x: Synaptic weights of the neuron, vec3.
     :param w: The input of the neural network
-    :param active:
+    :param active: Tell if the
     :return:
     """
-    seuil = w[0]
-    dot_product = np.dot(x, w[1:])
-    res = dot_product + seuil
+    seuil : float = w[0]
+    dot_product : float = np.dot(x, w[1:])
+    res : float = dot_product + seuil
     return np.sign(res) if (active == 0) else np.tanh(res)
