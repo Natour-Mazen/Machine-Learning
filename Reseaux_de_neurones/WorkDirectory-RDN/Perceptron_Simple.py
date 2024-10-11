15ec047c8150e893653f7637cdf5c502b10abc19
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-10-11 11:00:22 +0200

    Update Perceptron_Simple.py

--- Reseaux_de_neurones/WorkDirectory-RDN/Perceptron_Simple.py
+++ Reseaux_de_neurones/WorkDirectory-RDN/Perceptron_Simple.py
@@ -51,26 +51,6 @@ def plot_with_class(data_points, weights, classes, title, min_y, max_y):
     return plt
 
 
-# =========================================================================
-
-# Widrow-hof Learning.
-def apprentissage_widrow(x: np.ndarray, yd: np.array, epoch: int, batch_size: int) -> (np.array, np.array):
-    """
-    Widrow-hof learning.
-
-    :param x: Input values, mat[2, n]
-    :param yd: Result for the input values, vec[n].
-    :param epoch: The number of loop on the training set.
-    :param batch_size: The number of traited values before updating the weights.
-    :return:
-    """
-    pass
-
-
-# f(x) = activation function
-# f'(x) = 1 - f(x)^2
-# new value = old value - learningRate * (-(targetValue - f(w * x) * f'(w * x) * x(j)))
-
 if __name__ == '__main__':
     print("1.1 - Classification by simple perceptron on OR dataset")
     weights_OR = np.array([-0.5, 1, 1])
