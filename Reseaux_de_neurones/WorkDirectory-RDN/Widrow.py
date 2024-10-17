fb99a18b75fb821259091ba6454b3aa79b56512b
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-10-17 12:26:23 +0200

    Update Widrow.py
    leo chanhge some things
    
    Co-Authored-By: Léo H <108589080+leoHerv@users.noreply.github.com>

--- Reseaux_de_neurones/WorkDirectory-RDN/Widrow.py
+++ Reseaux_de_neurones/WorkDirectory-RDN/Widrow.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from Perceptron_Simple import perceptron_simple, plot_with_class
-from Plot_Display import plot_multiple
-
-# f(x) = activation function
-# f'(x) = 1 - f(x)^2
-# new value = old value - learningRate * (-(targetValue - f(w * x) * f'(w * x) * x))
-# newW = w - lr * (-(yd[0] - np.sign(w * x[0]) * derive(np.sign, w * x[0]) * x[0]))
+from Plot_Display import plot_multiple, plot_error_curve
 
 DataSetName = ''
+
+
+def tanhDerive(value: float) -> float:
+    """
+    The derivative of the tanh function.
+    :param value: The value to evaluate.
+    :return: The result of the derivative.
+    """
+    return 1 - np.tanh(value) ** 2
 
 
 # Widrow-hof Learning.
@@ -31,14 +35,53 @@ def apprentissage_widrow(inputs, targets, epochs, batch_size):
 
     for epoch in range(epochs):
         temp_weights = weights.copy()
-        epoch_error = 0
+        epoch_error: float = 0.
 
-        for i, input_vec in enumerate(inputs):
-            output = perceptron_simple(input_vec, weights, 1)
-            error = - (targets[i] - output) * (1 - output * output)
-            temp_weights += learning_rate * error * np.array([1, input_vec[0], input_vec[1]])
-            epoch_error += error ** 2
+        # Combined inputs and targets into tuples.
+        combined_inputs_targets = list(zip(inputs, targets))
 
+        # We shuffle the combined list to have better results.
+        np.random.shuffle(combined_inputs_targets)
+
+        # We unzip the combined list back into inputs and targets.
+        shuffle_inputs, shuffle_targets = zip(*combined_inputs_targets)
+
+        # Convert them back to array.
+        shuffle_inputs = np.array(inputs)
+        shuffle_targets = np.array(targets)
+
+        # For all the elements, in input_vec we have an array with 2 values : [v1, v2].
+        for i, input_vec in enumerate(shuffle_inputs):
+            output: float = perceptron_simple(input_vec, weights, 1)
+
+            # We transform the output between -1 and 1 in just -1 or 1.
+            output = np.sign(output)
+
+            # We insert 1. at the start of input vector because we need a value to calculate the first weight.
+            input_vec = np.insert(input_vec, 0, 1.)
+
+            # The value pass to the activation function.
+            value_activation: float = np.dot(temp_weights, input_vec)
+            # Value through the tanh function.
+            tanh_result: float = np.tanh(value_activation)
+            # Value through the derivative of the tanh function.
+            tanh_derive_result: float = tanhDerive(value_activation)
+
+            # Formula to have the new weight :
+            # newW = w - lr * (-(yd[i] - tanh(w * x) * tanh_derive(w * x) * x[i]))
+
+            # Update the weights.
+            temp_weights[0] = temp_weights[0] - learning_rate * (
+                        -(shuffle_targets[i] - tanh_result) * tanh_derive_result * input_vec[0])
+            temp_weights[1] = temp_weights[1] - learning_rate * (
+                        -(shuffle_targets[i] - tanh_result) * tanh_derive_result * input_vec[1])
+            temp_weights[2] = temp_weights[2] - learning_rate * (
+                        -(shuffle_targets[i] - tanh_result) * tanh_derive_result * input_vec[2])
+
+            # We calculate the error.
+            epoch_error += (shuffle_targets[i] - output) ** 2
+
+            # If we have process 'batch_size' elements, we change the weights.
             if (i + 1) % batch_size == 0:
                 weights = temp_weights
 
@@ -46,10 +89,11 @@ def apprentissage_widrow(inputs, targets, epochs, batch_size):
         print(f"Epoch {epoch + 1} : {epoch_error}")
 
         if epoch % step == 0:
-            plots_data.append((inputs, weights, targets))
-            plots_titles.append(f"{DataSetName} : Epoch {epoch + 1}")
+            plots_data.append((shuffle_inputs, weights, shuffle_targets))
+            plots_titles.append(f"{DataSetName} : Epoch {epoch + 1}, Error {epoch_error}")
 
-        if epoch_error == 0 or (epoch > 0 and errors[epoch - 1] - epoch_error == 0):
+        # If we have an error egal to 0, we stop because it can't be better.
+        if epoch_error == 0.:
             break
 
     plot_multiple(plots_data, plots_titles)
@@ -68,7 +112,8 @@ def run_widrow_learning(data_file, title, epochs, batch_size):
     global DataSetName
     data = np.loadtxt(data_file)
     targets = [1] * 25 + [-1] * 25
-    plt.title(title)
+    title_points = f"Distribution of points - {data_file}"
+    plt.title(title_points)
     plt.scatter(data[0, :25], data[1, :25], c='r')
     plt.scatter(data[0, 25:], data[1, 25:], c='b')
     plt.legend(['Classe 1', 'Classe 2'])
@@ -78,12 +123,14 @@ def run_widrow_learning(data_file, title, epochs, batch_size):
     weights, errors = apprentissage_widrow(data.T, targets, epochs, batch_size)
     print("Weights : ", weights)
 
-    plt.title(f"Errors {title} depending on the number of iterations")
+    plot_error_curve(errors, data_file)
+
+    plt.title(f"Errors depending on the number of epochs - {data_file}")
     plt.plot(errors)
     plt.show()
 
 
 if __name__ == '__main__':
     print("1.2.1 - Widrow-hoff learning programming")
-    run_widrow_learning('ressources/p2_d1.txt', "1.2.1 Learning Widrow-hoff on Data_p2_d1", 500, 25)
-    run_widrow_learning('ressources/p2_d2.txt', "1.2.1 Learning Widrow-hoff on Data_p2_d2", 800, 25)
+    run_widrow_learning('ressources/p2_d1.txt', "1.2.1 - Widrow on Data_p2_d1", 10, 8)
+    run_widrow_learning('ressources/p2_d2.txt', "1.2.1 - Widrow on Data_p2_d2", 15, 10)
