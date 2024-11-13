d06a54f733319059e901835afc5a5c32e77e9bfa
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-11-13 15:17:23 +0100

    Update main.py

--- Apprentissage_par_renforcement/WorkDirectory-RL/main.py
+++ Apprentissage_par_renforcement/WorkDirectory-RL/main.py
@@ -1,9 +1,14 @@
 # Description: This file is the main file that will be run to start the game.
-from Game import RLGame
+from Enums.Moves import Moves
+from RLGame import RLGame
 
+# Initialisation du jeu
 game = RLGame.define_basic_game()
 game.display_board()
 
-# Test an action
-new_position, reward, game_over = game.apply_action(1)  # Action "down"
+# Plateau de jeu actuel
+space = game.board  # L'organisation du plateau est représentée par game.board
+
+# Test d'une action
+new_position, reward, game_over = game.apply_action( Moves.DOWN, space, {})  # Action "down"
 print(f"New Position: {new_position}, Reward: {reward}, Game Over: {game_over}")
