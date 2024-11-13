cc870d67c44bf680dd86ebd18749006e906d3127
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-11-13 15:16:33 +0100

    Enums
    add enums to update the game more easier

---
+++ Apprentissage_par_renforcement/WorkDirectory-RL/Enums/Rewards.py
@@ -0,0 +1,7 @@
+# rewards.py
+from enum import Enum
+
+class Rewards(Enum):
+    NORMAL = -1
+    ENEMIES = -10
+    END = 0
---
+++ Apprentissage_par_renforcement/WorkDirectory-RL/Enums/Symbols.py
@@ -0,0 +1,9 @@
+# Symbols.py
+from enum import Enum
+
+class Symbols(Enum):
+    EMPTY = '.'
+    START = 'S'
+    END = 'E'
+    DRAGON = 'D'
+    PLAYER = 'P'
