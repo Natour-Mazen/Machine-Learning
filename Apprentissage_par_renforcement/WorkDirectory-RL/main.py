de152ba1f276c19c836883159d940625ca660211
Al Natour Mazen <mazen.natour.pro@gmail.com>
2024-12-13 15:19:39 +0100

    Project-3 ML
    fix the main

--- Apprentissage_par_renforcement/WorkDirectory-RL/main.py
+++ Apprentissage_par_renforcement/WorkDirectory-RL/main.py
@@ -6,25 +6,44 @@ from GameGUI import GameGUI
 from Q_learning import q_learning
 from RLGame import RLGame
 from Deep_Q_learning import q_deep_learning, choose_action
-from GameGUI import GameGUI
 
 if __name__ == '__main__':
-    # Parameters for Q-learning
-    alpha = 0.9
-    gamma = 0.5
-    # episodes = 1000 # Simple rewards
-    episodes = 100 # More efficient rewards
+    ####################################
+    ##    Parameters for Q-learning   ##
+    ####################################
+    # alpha = 0.9
+    # gamma = 0.5
+    # # episodes = 1000 # Simple rewards
+    # episodes = 100 # More efficient rewards
+
+
+
+    ####################################
+    ## Parameters for Deep Q-learning ##
+    ####################################
+    gamma = 0.999
+    episodes = 10 # More efficient rewards
+
+
+
+    #######################################
+    ##         General rewards           ##
+    #######################################
     rewards_q_learning = {'normal': Rewards.NORMAL.value,
-                          'dragon': Rewards.ENEMIES.value,
-                          'end': Rewards.END.value,
-                          'wall': Rewards.WALL.value}
+                           'dragon': Rewards.ENEMIES.value,
+                           'end': Rewards.END.value,
+                           'wall': Rewards.WALL.value}
 
-    # Initialize environment
-    # env = RLGame.define_basic_game()
 
-    # game_gui = GameGUI(env)
-    #
-    # # Run Q-learning
+    ##############################################
+    ## Initialize environment with the Game GUI ##
+    ##############################################
+    env = RLGame.define_basic_game()
+    game_gui = GameGUI(env)
+
+    #######################################
+    ##          Run Q-learning           ##
+    #######################################
     # Q = q_learning(env, episodes, alpha, gamma, rewards_q_learning, game_gui)
     #
     # while True:
@@ -36,17 +55,11 @@ if __name__ == '__main__':
     #     pygame.display.update()
     #     pygame.time.delay(100)
 
-   # pygame.quit()
 
-    # Parameters for Deep Q-learning
-    gamma = 0.999
-    episodes = 1000 # More efficient rewards
 
-    # Initialize environment
-    env = RLGame.define_basic_game()
-
-    game_gui = GameGUI(env)
-
+    #######################################
+    ##      Run Deep Q-learning          ##
+    #######################################
     trained_model = q_deep_learning(env, episodes, gamma, rewards_q_learning, game_gui)
 
     env_test = RLGame.define_basic_game()
@@ -74,3 +87,5 @@ if __name__ == '__main__':
         print(f"Action: {action}, Reward: {reward}")
         print("===  ===")
         game_gui.update_display(env_test.player_position, next_position, wall_hit, action, reward, Q)
+
+    # pygame.quit()
